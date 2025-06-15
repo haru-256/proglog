f10764b42eeabc485866589474e5ebc6253c9e8a
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-06-15 23:19:02 +0900

    fix: Update GRPCStatus to use correct OutOfRange code for ErrOffsetOutOfRange

--- workbench/api/v1/error.go
+++ workbench/api/v1/error.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
@@ -12,7 +13,7 @@ type ErrOffsetOutOfRange struct {
 }
 
 func (e ErrOffsetOutOfRange) GRPCStatus() *status.Status {
-	st := status.New(404, fmt.Sprintf("offset out of range: %d", e.Offset))
+	st := status.New(codes.OutOfRange, fmt.Sprintf("offset out of range: %d", e.Offset))
 	msg := fmt.Sprintf("The requested offset is outside the log's range: %d", e.Offset)
 
 	d := errdetails.LocalizedMessage{
