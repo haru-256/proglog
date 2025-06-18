982700e199041071399cd87fec28d3d61fcff274
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-06-18 23:39:02 +0900

    Implement authorization for gRPC server using Casbin
    - Added Authorizer struct and methods for authorization in authorizer.go.
    - Updated server.go to include authorization checks for Produce and Consume methods.
    - Integrated gRPC middleware for authentication and authorization.
    - Modified server_test.go to include tests for unauthorized access.
    - Added configuration for ACL model and policy files in files.go.
    - Created model.conf and policy.csv for defining access control rules.
    - Added client CSR configuration for testing purposes.

--- workbench/internal/server/server.go
+++ workbench/internal/server/server.go
@@ -5,8 +5,19 @@ import (
 	"context"
 
 	api "github.com/haru-256/proglog/api/v1"
+
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
 )
+
+type Authorizer interface {
+	Authorize(subject, object, action string) error
+}
 
 // CommitLog defines the interface for interacting with the commit log.
 // It provides methods for appending records and reading records by offset.
@@ -20,13 +31,30 @@ type CommitLog interface {
 // Config holds the configuration for the gRPC server.
 // It includes a CommitLog instance for log operations.
 type Config struct {
-	CommitLog CommitLog // The underlying commit log implementation
+	CommitLog  CommitLog // The underlying commit log implementation
+	Authorizer Authorizer
 }
+
+const (
+	objectWildcard = "*"
+	produceAction  = "produce"
+	consumeAction  = "consume"
+)
 
 // NewGRPCServer creates and configures a new gRPC server.
 // It initializes the server, registers the log service, and returns the server instance.
 // The server uses the provided configuration and optional gRPC server options (e.g., TLS settings).
 func NewGRPCServer(config *Config, grpcOpts ...grpc.ServerOption) (*grpc.Server, error) {
+	grpcOpts = append(
+		grpcOpts,
+		grpc.StreamInterceptor(
+			grpc_middleware.ChainStreamServer(
+				grpc_auth.StreamServerInterceptor(authenticate),
+			)),
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_auth.UnaryServerInterceptor(authenticate),
+		)),
+	)
 	gsrv := grpc.NewServer(grpcOpts...)
 	srv, err := newgrpcServer(config)
 	if err != nil {
@@ -57,6 +85,10 @@ func newgrpcServer(config *Config) (srv *grpcServer, err error) {
 // Produce handles a single produce request.
 // It appends the provided record to the commit log and returns the offset.
 func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	if err := s.Authorizer.Authorize(subject(ctx), objectWildcard, produceAction); err != nil {
+		return nil, err
+	}
+
 	offset, err := s.CommitLog.Append(req.Record)
 	if err != nil {
 		return nil, err
@@ -67,6 +99,10 @@ func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api
 // Consume handles a single consume request.
 // It reads a record from the commit log at the specified offset.
 func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	if err := s.Authorizer.Authorize(subject(ctx), objectWildcard, consumeAction); err != nil {
+		return nil, err
+	}
+
 	record, err := s.CommitLog.Read(req.Offset)
 	if err != nil {
 		return nil, err
@@ -119,3 +155,32 @@ func (s *grpcServer) ConsumeStream(req *api.ConsumeRequest, stream api.Log_Consu
 		}
 	}
 }
+
+func authenticate(ctx context.Context) (context.Context, error) {
+	// gRPCサーバーがクライアントからRPCリクエストを受け取ると、gRPCフレームワークは自動的にそのクライアントの接続情報を context.Context に格納する
+	// リクエストを送信してきたクライアント（Peer）の接続情報を取得
+	peer, ok := peer.FromContext(ctx)
+	if !ok {
+		return ctx, status.New(codes.Unknown, "couldn't get peer info").Err()
+	}
+	if peer.AuthInfo == nil {
+		return context.WithValue(ctx, subjectContextKey{}, ""), nil
+	}
+	tlsInfo := peer.AuthInfo.(credentials.TLSInfo)
+	// VerifiedChainsが空 = クライアント証明書が検証されていない
+	// VerifiedChains[0] が1つ目の検証済みチェーン
+	// VerifiedChains[0][0] がそのチェーンのリーフ証明書（クライアント自身の証明書）
+	if len(tlsInfo.State.VerifiedChains) == 0 || len(tlsInfo.State.VerifiedChains[0]) == 0 {
+		return ctx, status.New(codes.Unauthenticated, "no verified chains found").Err()
+	}
+	// サーバーによる検証が済んだVerifiedChainsからアイデンティティを取得
+	subject := tlsInfo.State.VerifiedChains[0][0].Subject.CommonName
+	ctx = context.WithValue(ctx, subjectContextKey{}, subject)
+	return ctx, nil
+}
+
+func subject(ctx context.Context) string {
+	return ctx.Value(subjectContextKey{}).(string)
+}
+
+type subjectContextKey struct{}
